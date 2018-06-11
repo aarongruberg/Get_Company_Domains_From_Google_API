0a28eaccebd0e427a4ec57750f5ae3a631c6478c
Aaron Gruberg <agruberg@DB-MBP-22G8WP.local>
2018-06-11 10:40:38 -0700

    Removed unecessary query to API.

--- getCompanyURL.py
+++ getCompanyURL.py
@@ -13,40 +13,15 @@ api_key = 'AIzaSyCTVEnopRQIgUkS0BunQ3pQFq64FnYP2O4'
 service_url = 'https://kgsearch.googleapis.com/v1/entities:search'
 
 # Read input list file
-f = 'EastRegionACQ_top100.csv'
+f = 'sampleList.csv'
 df = pd.read_csv(f)
-#df = df.head(13)
-
-#-------------------------------------------------------------------------------------------------------------
-
-def getAPI_Name(name):
-
-	query = name
-	params = {
-			'query': query,
-			'limit': 1,
-			'indent': True,
-			'key': api_key,
-	}
-
-	url = service_url + '?' + urllib.urlencode(params)
-	response = json.loads(urllib.urlopen(url).read())
-
-	for element in response['itemListElement']:
-		if 'name' in element['result'].keys():
-			element['result']['name'] = element['result']['name'].encode("utf-8")
-			return element['result']['name']
-
-
-# Call getAPI_Name() on a dataframe column using .apply()
-# Planning to vectorize pandas series to speed up runtime.
-df['API_names'] = df['Account Name'].apply(getAPI_Name)
+#df = df.head()
 
 #------------------------------------------------------------------------------------------------------------
 
-def getAPI_Domain(api_name):
+def getAPI_Domain(company_name):
 
-	query = api_name
+	query = company_name
 	params = {
 			'query': query,
 			'limit': 1,
@@ -59,13 +34,13 @@ def getAPI_Domain(api_name):
 
 	for element in response['itemListElement']:
 		if 'url' in element['result'].keys():
-			if element['result']['name'] == query:
-				element['result']['url'] = element['result']['url'].encode("utf-8")
-				return element['result']['url']
+			element['result']['url'] = element['result']['url'].encode("utf-8")
+			return element['result']['url']
 
+#-------------------------------------------------------------------------------------------------------------
 
-df['Domain'] = df['API_names'].apply(getAPI_Domain)
-df = df.drop(['API_names'], axis = 1)
+# calls getAPI_Domain() on each 'Account Name'
+df['Domain'] = df['Account Name'].apply(getAPI_Domain)
 
 # What % of domains were retrieved
 num_names = df['Account Name'].count()
@@ -75,10 +50,7 @@ percent = ratio * 100
 print "'%' of domains retrieved:", round(percent, 2)
 
 
-df.to_csv('EastRegionACQ_top100_input.csv', index = False)
-
-
-
+df.to_csv('sampleListInput.csv', index = False)
 
 
 
